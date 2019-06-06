796037c7c013487efbdc5bff0eb1996fa542dd3d
degah <HeshmaReddy.Dega@in.pega.com>
2019-06-06 17:47:22 +0530

    moving the source statements inside condition

--- scripts/installer_utils.sh
+++ scripts/installer_utils.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-source /opt/pega/kit/scripts/installer_pre_upgrade.sh
-source /opt/pega/kit/scripts/installer_post_upgrade.sh
-
 do_upgrade_pre_post_actions() {
 
   if [ "$ACTION" == 'pre-upgrade' ]; then
+    source /opt/pega/kit/scripts/installer_pre_upgrade.sh
     do_pre_upgrade_actions
   elif [ "$ACTION" == 'post-upgrade' ]; then
+    source /opt/pega/kit/scripts/installer_post_upgrade.sh
     do_post_upgrade_actions
   else
     echo "Invalid action " $ACTION " passed.";
