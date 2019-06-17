a310b85703b2821dc62cc0301536c935974b6685
koyateja98github <51357150+koyateja98github@users.noreply.github.com>
2019-06-17 14:37:16 +0530

    Add files via upload

---
+++ scripts/docker-entrypoint.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#  Contains Commaon variables
+source /scripts/variables.sh
+
+#  Contains Common Funtion definitions
+source /scripts/common_functions.sh
+
+mount_files()
+{
+
+	# mount these files before mounting/dockerizing prconfig, prbootstrap, setupdatabase
+	mount_log4j2
+	mount_dbconf
+}
+
+
+dockerizeFiles()
+{
+
+	dockerizePrconfig
+	dockerizePrbootstrap
+	dockerizeSetupdatabase
+
+}
+execute()
+{
+
+	# Check action validation
+	isActionValid
+
+
+        if [ -z "$(ls -A /opt/pega/kit/scripts)" -a -z "$(ls -A /opt/pega/kit/archives)" -a -z "$(ls -A /opt/pega/kit/rules)" ]
+        then	
+	    # unzip distribution kit
+	    unzipKit
+	fi
+
+
+	# intialize the jar path and secrets before mounting any config file
+	constructJarPath
+	readSecrets
+	mount_files
+	initializeSchemas
+	dockerizeFiles
+
+	# setupdatabase need to be mounted or dockerized for generateconfig to work
+	generateConfig
+
+
+	if [ "$ACTION" == 'install' ] || [ "$ACTION" == 'install-deploy' ]; then
+	  #------------------------INSTALL-------------------------------------
+	  source /scripts/pega_install.sh
+	elif [ "$ACTION" == 'upgrade' ] || [ "$ACTION" == 'upgrade-deploy' ]; then
+	  #---------------------------UPGRADE----------------------------------
+	  source /scripts/pega_upgrade.sh
+	elif [ "$ACTION" == 'pre-upgrade' ]; then
+	  source /scripts/pega_pre_upgrade.sh
+	elif [ "$ACTION" == 'post-upgrade' ]; then
+	  source /scripts/pega_post_upgrade.sh
+	else
+	  echo "Invalid action " $ACTION " passed.";
+	  exit 1;
+	fi
+	
+	executeInner
+
+}
+execute
+
